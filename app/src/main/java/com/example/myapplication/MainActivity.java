cfe565d7fe9bde8cc2c7decb020a572209572bb2
James <james@theinnovationguys.com>
2024-07-29 18:32:05 +0700

    1111

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mylibrary.License;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        License login = new License();
+        login.GetLicense(getApplicationContext());
+
+
+        try {
+            Intent intent = new Intent(getApplicationContext(),com.example.mylibrary.Register.class);
+            startActivity(intent);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/License.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class License {
+
+    public void GetLicense(Context context){
+        Toast.makeText(context, "Get License From Lib", Toast.LENGTH_LONG).show();
+    }
+}
